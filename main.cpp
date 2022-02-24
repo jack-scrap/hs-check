63302100206f36cb4abdbfcf47b36131e98c1bc1
Jack <jackhasakeyboard@gmail.com>
2022-02-24 10:17:16 -0700

    Bound, unbound proper handles upon drawing. Shader made into member to be able to do so

--- main.cpp
+++ main.cpp
@@ -33,59 +33,66 @@ class Piece {
 
 		GLuint _ibo;
 
+		Prog _prog;
+
 	public:
-		Piece() {
-			// data
-			glGenVertexArrays(1, &_vao);
-			glBindVertexArray(_vao);
+		Piece() :
+			_prog("obj", "solid") {
+				// data
+				glGenVertexArrays(1, &_vao);
+				glBindVertexArray(_vao);
 
-			// position
-			glGenBuffers(1, &_vbo);
-			glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+				// position
+				glGenBuffers(1, &_vbo);
+				glBindBuffer(GL_ARRAY_BUFFER, _vbo);
 
-			float stride = rot / _n;
+				float stride = rot / _n;
 
-			for (int i = 0; i < _n; i++) {
-				_vtc[(i * 3)] = sin(i * stride);
-				_vtc[(i * 3) + 1] = cos(i * stride);
-				_vtc[(i * 3) + 2] = 0.0;
+				for (int i = 0; i < _n; i++) {
+					_vtc[(i * 3)] = sin(i * stride);
+					_vtc[(i * 3) + 1] = cos(i * stride);
+					_vtc[(i * 3) + 2] = 0.0;
+				}
+
+				_vtc[_n * 3] = 0.0;
+				_vtc[(_n * 3) + 1] = 0.0;
+				_vtc[(_n * 3) + 2] = 0.0;
+
+				glBufferData(GL_ARRAY_BUFFER, sizeof _vtc, _vtc, GL_STATIC_DRAW);
+
+				// index
+				glGenBuffers(1, &_ibo);
+				glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _ibo);
+
+				glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof _idc, _idc, GL_STATIC_DRAW);
+
+				/// attribute
+				GLint attrPos = glGetAttribLocation(_prog._id, "pos");
+				glVertexAttribPointer(attrPos, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*) 0);
+				glEnableVertexAttribArray(attrPos);
+
+				/// uniform
+				GLint uniModel = glGetUniformLocation(_prog._id, "model");
+				GLint uniView = glGetUniformLocation(_prog._id, "view");
+				GLint uniProj = glGetUniformLocation(_prog._id, "proj");
+
+				// initialize
+				_prog.use();
+
+				glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(_model));
+				glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(_view));
+				glUniformMatrix4fv(uniProj, 1, GL_FALSE, glm::value_ptr(_proj));
 			}
 
-			_vtc[_n * 3] = 0.0;
-			_vtc[(_n * 3) + 1] = 0.0;
-			_vtc[(_n * 3) + 2] = 0.0;
-
-			glBufferData(GL_ARRAY_BUFFER, sizeof _vtc, _vtc, GL_STATIC_DRAW);
-
-			// index
-			glGenBuffers(1, &_ibo);
-			glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _ibo);
-
-			glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof _idc, _idc, GL_STATIC_DRAW);
-
-			// shader
-			Prog prog("obj", "solid");
-
-			/// attribute
-			GLint attrPos = glGetAttribLocation(prog._id, "pos");
-			glVertexAttribPointer(attrPos, 3, GL_FLOAT, GL_FALSE, 0, (GLvoid*) 0);
-			glEnableVertexAttribArray(attrPos);
-
-			/// uniform
-			GLint uniModel = glGetUniformLocation(prog._id, "model");
-			GLint uniView = glGetUniformLocation(prog._id, "view");
-			GLint uniProj = glGetUniformLocation(prog._id, "proj");
-
-			// initialize
-			prog.use();
-
-			glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(_model));
-			glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(_view));
-			glUniformMatrix4fv(uniProj, 1, GL_FALSE, glm::value_ptr(_proj));
-		}
-
 		void draw() {
+			glBindVertexArray(_vao);
+			_prog.use();
+
 			glDrawElements(GL_TRIANGLES, sizeof _idc / sizeof *_idc, GL_UNSIGNED_INT, (GLvoid*) 0);
+
+			_prog.unUse();
+			glBindVertexArray(0);
+
 		}
 };
 
