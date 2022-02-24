25ffa2fb3567cef81de2b22f21b01d28d1123dcd
Jack <jackhasakeyboard@gmail.com>
2022-02-24 10:07:45 -0700

    Added purple

--- shad.fs
+++ shad.fs
@@ -1,5 +1,8 @@
 #version 400
 
+vec3 inert = vec3(69, 59, 87);
+vec3 active = vec3(97, 82, 134);
+
 void main() {
-	gl_FragColor = vec4(vec3(1.0), 1.0);
+	gl_FragColor = vec4(active / 255.0, 1.0);
 }
