85b8f40530f9bc37af8f4cd5d6dc17ab1b0ff967
Jack <jackhasakeyboard@gmail.com>
2022-02-24 10:52:28 -0700

    Refactored

--- res/solid.fs
+++ res/solid.fs
@@ -1,8 +1,10 @@
 #version 400
 
-vec3 inert = vec3(69, 59, 87);
-vec3 active = vec3(97, 82, 134);
+vec3[2] purple = vec3[2](
+	vec3(69, 59, 87),
+	vec3(97, 82, 134)
+);
 
 void main() {
-	gl_FragColor = vec4(active / 255.0, 1.0);
+	gl_FragColor = vec4(purple[1] / 255.0, 1.0);
 }
