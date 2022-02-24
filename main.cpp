27d64325f0a9b5432781ff20fa37be6263824bc9
Jack <jackhasakeyboard@gmail.com>
2022-02-24 10:10:45 -0700

    Explicitly specified values as floating-point

--- main.cpp
+++ main.cpp
@@ -56,7 +56,7 @@ int main() {
 
 	// matrix
 	glm::mat4 model = glm::mat4(1.0);
-	glm::mat4 view = glm::lookAt(glm::vec3(3, 3, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
+	glm::mat4 view = glm::lookAt(glm::vec3(3.0, 3.0, 3.0), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0, 1, 0));
 	glm::mat4 proj = glm::perspective(glm::radians(45.0), 800.0 / 600.0, 0.1, 100.0);
 
 	// shader
