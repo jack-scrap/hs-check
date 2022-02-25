1b085285bd1540f25f230b48f9c5224938ad3ba7
Jack <jackhasakeyboard@gmail.com>
2022-02-25 04:46:55 -0700

    Modulated checkerboard on one axis

--- res/board.fs
+++ res/board.fs
@@ -1,10 +1,17 @@
 #version 400
 
+in vec3 _pos;
+
+const float pad = 0.16;
+const float sz = 2.0;
+
+const float stride = sz + (pad * 2);
+
 vec3[2] grey = vec3[2](
 	vec3(34, 34, 34),
 	vec3(102, 102, 102)
 );
 
 void main() {
-	gl_FragColor = vec4(grey[0] / 225.0, 1.0);
+	gl_FragColor = vec4(grey[int((mod(_pos.x, stride * 2)) > stride)] / 225.0, 1.0);
 }
