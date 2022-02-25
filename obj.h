e35dc73501d8b2e2c8cf5da57d1a30103c68f49a
Jack <jackhasakeyboard@gmail.com>
2022-02-25 05:12:52 -0700

    Moved camera back, something reasonable that can encompass the entirety of the board given perspective projection

--- obj.h
+++ obj.h
@@ -10,7 +10,7 @@ class Obj {
 		unsigned int _noEl;
 
 		glm::mat4 _model = glm::mat4(1.0);
-		glm::mat4 _view = glm::lookAt(glm::vec3(20.0, 20.0, 20.0), glm::vec3(0.0), glm::vec3(0, 1, 0));
+		glm::mat4 _view = glm::lookAt(glm::vec3(30.0, 30.0, 30.0), glm::vec3(0.0), glm::vec3(0, 1, 0));
 		glm::mat4 _proj = glm::perspective(glm::radians(45.0), 800.0 / 600.0, 0.1, 10000.0);
 
 		GLuint _id[3];
