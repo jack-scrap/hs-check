30ee045a4be56a53b106dea7e4b8aea82a216bf1
Jack <jackhasakeyboard@gmail.com>
2022-02-26 03:07:09 -0700

    Moved containers of data out of definition, vertices generated programmatically and indices intended to be

--- piece.cpp
+++ piece.cpp
@@ -18,20 +18,42 @@ Piece::Piece(unsigned int* pos, glm::vec3 loc, glm::vec3 rot, bool active) :
 
 		float stride = _rot / _n;
 
+		GLfloat vtc[((8 * 3) * 2) + 3];
+
 		int teamSz = 8 * 3;
 		for (int b = 0; b < 2; b++) {
 			for (int i = 0; i < _n; i++) {
-				_vtc[(b * teamSz) + (i * 3)] = sin(i * stride);
-				_vtc[(b * teamSz) + (i * 3) + 1] = cos(i * stride);
-				_vtc[(b * teamSz) + (i * 3) + 2] = b * 0.2;
+				vtc[(b * teamSz) + (i * 3)] = sin(i * stride);
+				vtc[(b * teamSz) + (i * 3) + 1] = cos(i * stride);
+				vtc[(b * teamSz) + (i * 3) + 2] = b * 0.2;
 			}
 		}
 
-		_vtc[_n * 2 * 3] = 0.0;
-		_vtc[(_n * 2 * 3) + 1] = 0.0;
-		_vtc[(_n * 2 * 3) + 2] = 0.0;
+		vtc[_n * 2 * 3] = 0.0;
+		vtc[(_n * 2 * 3) + 1] = 0.0;
+		vtc[(_n * 2 * 3) + 2] = 0.0;
 
-		glBufferData(GL_ARRAY_BUFFER, sizeof _vtc, _vtc, GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, sizeof vtc, vtc, GL_STATIC_DRAW);
+
+		GLushort idc[8 * 3 * 2] = {
+			0, 1, _n,
+			1, 2, _n,
+			2, 3, _n,
+			3, 4, _n,
+			4, 5, _n,
+			5, 6, _n,
+			6, 7, _n,
+			7, 8, _n,
+
+			8, 9, _n,
+			9, 10, _n,
+			10, 11, _n,
+			11, 12, _n,
+			12, 13, _n,
+			13, 14, _n,
+			14, 15, _n,
+			15, 0, _n
+		};
 
 		// matrix
 		_model = glm::translate(_model, loc);
@@ -46,7 +68,7 @@ Piece::Piece(unsigned int* pos, glm::vec3 loc, glm::vec3 rot, bool active) :
 		// index
 		glGenBuffers(1, &_id[IBO]);
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _id[IBO]);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof _idc, _idc, GL_STATIC_DRAW);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof idc, idc, GL_STATIC_DRAW);
 
 		// attribute
 		_attr[POS] = glGetAttribLocation(_prog._id, "pos");
@@ -74,7 +96,7 @@ void Piece::draw() {
 	glBindVertexArray(_id[VAO]);
 	_prog.use();
 
-	glDrawElements(GL_TRIANGLES, sizeof _idc / sizeof *_idc, GL_UNSIGNED_SHORT, (GLvoid*) 0);
+	glDrawElements(GL_TRIANGLES, _n * 3 * 2, GL_UNSIGNED_SHORT, (GLvoid*) 0);
 
 	_prog.unUse();
 	glBindVertexArray(0);
--- piece.h
+++ piece.h
@@ -15,28 +15,6 @@ class Piece {
 
 		unsigned int _pos[2];
 
-		GLfloat _vtc[((8 * 3) * 2) + 3];
-
-		GLushort _idc[8 * 3 * 2] = {
-			0, 1, _n,
-			1, 2, _n,
-			2, 3, _n,
-			3, 4, _n,
-			4, 5, _n,
-			5, 6, _n,
-			6, 7, _n,
-			7, 8, _n,
-
-			8, 9, _n,
-			9, 10, _n,
-			10, 11, _n,
-			11, 12, _n,
-			12, 13, _n,
-			13, 14, _n,
-			14, 15, _n,
-			15, 0, _n
-		};
-
 		glm::mat4 _model = glm::mat4(1.0);
 		glm::mat4 _view = glm::lookAt(cam._loc, glm::vec3(0.0), glm::vec3(0, 1, 0));
 		glm::mat4 _proj = glm::perspective(glm::radians(45.0), 800.0 / 600.0, 0.1, 10000.0);
